62ac5a2aea466daad2a12d7266c36cdf3a955ade
PRATEEK PARWANI <f20220144@pilani.bits-pilani.ac.in>
2023-08-31 22:43:43 +0530

    update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{useNewUrlParser:true,useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://taskapp:<Prateek10>@cluster0.ssftsf9.mongodb.net/task-manager-api',{useNewUrlParser:true,useUnifiedTopology: true})
 
 
 
